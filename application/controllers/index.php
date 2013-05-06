1815ed8993a2e082ded70f223a3eebb7565cf182
陆秋石 <uicestone@gmail.com>
2013-05-06 21:09:44 +0800

    数据样例

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -8,7 +8,33 @@ class Index extends WT_Controller{
 	 * 首页
 	 */
 	function index(){
-		$this->load->view('index');
+		
+		$project=array(
+			array(
+				'id'=>1,
+				'pic'=>'/uploads/images/project/1.jpg',
+				'expire_date'=>'2012-09-05',
+				'bonus'=>'10000.00',
+				'labels'=>array('可乐','广告'),
+				'comments'=>array(
+					array('id'=>1,'content'=>'Nike的Air系列不错。','user'=>5,'username'=>'user_01'),
+					array('id'=>1,'content'=>'顶楼上。','user'=>6,'username'=>'user_01')
+				)
+			),
+			array(
+				'id'=>2,
+				'pic'=>'/uploads/images/project/2.jpg',
+				'expire_date'=>'2012-09-05',
+				'bonus'=>'10000.00',
+				'labels'=>array('可乐','广告'),
+				'comments'=>array(
+					array('id'=>1,'content'=>'Nike的Air系列不错。','user'=>5,'username'=>'user_01'),
+					array('id'=>1,'content'=>'顶楼上。','user'=>6,'username'=>'user_01')
+				)
+			)
+		);
+		
+		$this->load->view('index',compact('project'));
 	}
 	
 }
