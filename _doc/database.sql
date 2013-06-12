66308c6ae1485e9704b6803f8d3468214699fb74
陆秋石 <uicestone@gmail.com>
2013-06-12 16:28:30 +0800

    数据库结构更新

--- _doc/database.sql
+++ _doc/database.sql
@@ -3,18 +3,12 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: 127.0.0.1
--- 生成日期: 2013 年 06 月 10 日 03:36
+-- 生成日期: 2013 年 06 月 12 日 08:25
 -- 服务器版本: 5.5.30-log
 -- PHP 版本: 5.3.15
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- 数据库: `witower`
@@ -40,7 +34,8 @@ CREATE TABLE IF NOT EXISTS `company` (
 
 INSERT INTO `company` (`id`, `description`) VALUES
 (1, ''),
-(2, '');
+(2, ''),
+(3, '');
 
 -- --------------------------------------------------------
 
@@ -54,14 +49,15 @@ CREATE TABLE IF NOT EXISTS `config` (
   `value` text NOT NULL,
   PRIMARY KEY (`id`),
   KEY `item` (`item`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
 -- 转存表中的数据 `config`
 --
 
 INSERT INTO `config` (`id`, `item`, `value`) VALUES
-(1, 'recommended_project', '1');
+(1, 'recommended_project', '1'),
+(2, 'recommended_vote', '2');
 
 -- --------------------------------------------------------
 
@@ -83,7 +79,7 @@ CREATE TABLE IF NOT EXISTS `product` (
 
 INSERT INTO `product` (`id`, `name`, `company`) VALUES
 (1, '雪碧', 3),
-(2, 'Air系列运动鞋', 1);
+(2, 'Bouns系列运动鞋', 1);
 
 -- --------------------------------------------------------
 
@@ -98,7 +94,7 @@ CREATE TABLE IF NOT EXISTS `product_tag` (
   PRIMARY KEY (`id`),
   KEY `product` (`product`),
   KEY `tag` (`tag`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
 --
 -- 转存表中的数据 `product_tag`
@@ -106,7 +102,8 @@ CREATE TABLE IF NOT EXISTS `product_tag` (
 
 INSERT INTO `product_tag` (`id`, `product`, `tag`) VALUES
 (3, 2, 2),
-(4, 2, 1);
+(4, 2, 1),
+(5, 1, 5);
 
 -- --------------------------------------------------------
 
@@ -119,26 +116,57 @@ CREATE TABLE IF NOT EXISTS `project` (
   `product` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `summary` varchar(255) NOT NULL,
-  `date_start` date NOT NULL,
-  `date_end` date NOT NULL,
+  `wit_start` date NOT NULL,
+  `wit_end` date NOT NULL,
+  `vote_start` date NOT NULL,
+  `vote_end` date NOT NULL,
   `bonus` decimal(10,2) NOT NULL,
   `company` int(11) NOT NULL,
-  `participants` int(11) NOT NULL DEFAULT '0',
+  `witters` int(11) NOT NULL DEFAULT '0',
+  `voters` int(11) NOT NULL,
   `favorites` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
-  KEY `date_start` (`date_start`),
-  KEY `date_end` (`date_end`),
+  KEY `date_start` (`wit_start`),
+  KEY `date_end` (`wit_end`),
   KEY `user` (`company`),
   KEY `product` (`product`),
-  KEY `users` (`participants`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+  KEY `users` (`witters`),
+  KEY `vote_start` (`vote_start`),
+  KEY `vote_end` (`vote_end`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
 -- 转存表中的数据 `project`
 --
 
-INSERT INTO `project` (`id`, `product`, `name`, `summary`, `date_start`, `date_end`, `bonus`, `company`, `participants`, `favorites`) VALUES
-(1, 2, 'NikeAir运动鞋春季校园推广', 'Nike公司计划在2014年春季推出Air Max 2014款运动鞋', '2013-01-01', '2013-12-31', '5000.00', 1, 1, 1);
+INSERT INTO `project` (`id`, `product`, `name`, `summary`, `wit_start`, `wit_end`, `vote_start`, `vote_end`, `bonus`, `company`, `witters`, `voters`, `favorites`) VALUES
+(1, 2, 'Adidas Bouns运动鞋春季校园推广', 'Adidas公司计划在2014年春季推出Bouns Max 2014款运动鞋', '2013-01-01', '2013-06-30', '2013-07-09', '2013-08-09', '5000.00', 1, 1, 0, 1),
+(2, 1, '雪碧2013校园推广', '雪碧2013校园推广', '2013-01-01', '2013-06-04', '2013-06-07', '2013-07-07', '8000.00', 3, 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `project_candidate`
+--
+
+CREATE TABLE IF NOT EXISTS `project_candidate` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `candidate` int(11) NOT NULL,
+  `project` int(11) NOT NULL,
+  `votes` int(11) NOT NULL,
+  `score_wit` decimal(10,2) NOT NULL,
+  `score_company` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `project-candidate` (`project`,`candidate`),
+  KEY `candidate` (`candidate`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- 转存表中的数据 `project_candidate`
+--
+
+INSERT INTO `project_candidate` (`id`, `candidate`, `project`, `votes`, `score_wit`, `score_company`) VALUES
+(1, 5, 2, 3, '6.00', '5.00');
 
 -- --------------------------------------------------------
 
@@ -153,14 +181,40 @@ CREATE TABLE IF NOT EXISTS `project_tag` (
   PRIMARY KEY (`id`),
   KEY `project` (`project`),
   KEY `tag` (`tag`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
 -- 转存表中的数据 `project_tag`
 --
 
 INSERT INTO `project_tag` (`id`, `project`, `tag`) VALUES
-(1, 1, 4);
+(1, 1, 4),
+(2, 2, 4);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `project_vote`
+--
+
+CREATE TABLE IF NOT EXISTS `project_vote` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `project` int(11) NOT NULL,
+  `candidate` int(11) NOT NULL,
+  `voter` int(11) NOT NULL,
+  `votes` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `project-voter-candidate` (`project`,`voter`,`candidate`),
+  KEY `candidate` (`candidate`),
+  KEY `voter` (`voter`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- 转存表中的数据 `project_vote`
+--
+
+INSERT INTO `project_vote` (`id`, `project`, `candidate`, `voter`, `votes`) VALUES
+(1, 2, 5, 6, 3);
 
 -- --------------------------------------------------------
 
@@ -172,7 +226,7 @@ CREATE TABLE IF NOT EXISTS `tag` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
 --
 -- 转存表中的数据 `tag`
@@ -181,7 +235,8 @@ CREATE TABLE IF NOT EXISTS `tag` (
 INSERT INTO `tag` (`id`, `name`) VALUES
 (1, '运动'),
 (2, '服饰'),
-(4, '校园');
+(4, '校园'),
+(5, '饮料');
 
 -- --------------------------------------------------------
 
@@ -198,17 +253,18 @@ CREATE TABLE IF NOT EXISTS `user` (
   UNIQUE KEY `name` (`name`),
   UNIQUE KEY `email` (`email`),
   KEY `password` (`password`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 --
 -- 转存表中的数据 `user`
 --
 
 INSERT INTO `user` (`id`, `name`, `password`, `email`) VALUES
-(1, 'Nike', '123', 'market@nike.com'),
-(2, 'Adidas', '123', 'market@adidas.com'),
-(3, '可口可乐', '123', 'market@cococola.com'),
-(5, 'uicestone', '123', 'uicestone@gmail.com');
+(1, 'Adidas', '123', 'market@adidas.com'),
+(2, 'Nike', '123', 'market@nike.com'),
+(3, '百事', '123', 'market@pepsi.com'),
+(5, 'uicestone', '123', 'uicestone@gmail.com'),
+(6, 'mouse', '123', 'mouse@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -307,24 +363,6 @@ CREATE TABLE IF NOT EXISTS `user_profile` (
 -- --------------------------------------------------------
 
 --
--- 表的结构 `user_score`
---
-
-CREATE TABLE IF NOT EXISTS `user_score` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user` int(11) NOT NULL,
-  `project` int(11) NOT NULL,
-  `votes` int(11) NOT NULL,
-  `score_wit` decimal(10,2) NOT NULL,
-  `score_company` decimal(10,2) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user` (`user`),
-  KEY `project` (`project`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
 -- 表的结构 `user_status`
 --
 
@@ -375,14 +413,15 @@ CREATE TABLE IF NOT EXISTS `version` (
   KEY `user` (`user`),
   KEY `time` (`time`),
   KEY `wit` (`wit`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
 -- 转存表中的数据 `version`
 --
 
 INSERT INTO `version` (`id`, `wit`, `content`, `score_wit`, `score_company`, `user`, `time`) VALUES
-(1, 1, '这是一段文案', 0, 0, 5, 0);
+(1, 1, '这是一段文案', 7, 6, 5, 0),
+(2, 2, '文案内容文案内容文案内容文案内容文案内容文案内容文案内容文案内容文案内容文案内容文案内容文案内容文案内容文案内容文案内容文案内容文案内容文案内容', 6, 5, 5, 0);
 
 -- --------------------------------------------------------
 
@@ -427,14 +466,15 @@ CREATE TABLE IF NOT EXISTS `wit` (
   KEY `project` (`project`),
   KEY `user` (`user`),
   KEY `time` (`time`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
 -- 转存表中的数据 `wit`
 --
 
 INSERT INTO `wit` (`id`, `name`, `content`, `project`, `user`, `time`, `selected`) VALUES
-(1, '文案标题1', '这是一段文案', 1, 5, 0, 0);
+(1, '文案标题1', '这是一段文案', 1, 5, 0, 0),
+(2, '雪碧广告文案', '文案内容文案内容文案内容文案内容文案内容文案内容文案内容文案内容文案内容文案内容文案内容文案内容文案内容文案内容文案内容文案内容文案内容文案内容', 2, 5, 0, 0);
 
 --
 -- 限制导出的表
@@ -467,11 +507,26 @@ ALTER TABLE `project`
   ADD CONSTRAINT `project_ibfk_3` FOREIGN KEY (`company`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
+-- 限制表 `project_candidate`
+--
+ALTER TABLE `project_candidate`
+  ADD CONSTRAINT `project_candidate_ibfk_2` FOREIGN KEY (`project`) REFERENCES `project` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `project_candidate_ibfk_3` FOREIGN KEY (`candidate`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
 -- 限制表 `project_tag`
 --
 ALTER TABLE `project_tag`
   ADD CONSTRAINT `project_tag_ibfk_1` FOREIGN KEY (`project`) REFERENCES `project` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
   ADD CONSTRAINT `project_tag_ibfk_2` FOREIGN KEY (`tag`) REFERENCES `tag` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- 限制表 `project_vote`
+--
+ALTER TABLE `project_vote`
+  ADD CONSTRAINT `project_vote_ibfk_1` FOREIGN KEY (`project`) REFERENCES `project` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `project_vote_ibfk_2` FOREIGN KEY (`candidate`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `project_vote_ibfk_3` FOREIGN KEY (`voter`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
 -- 限制表 `user_bonus`
@@ -507,13 +562,6 @@ ALTER TABLE `user_profile`
   ADD CONSTRAINT `user_profile_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
--- 限制表 `user_score`
---
-ALTER TABLE `user_score`
-  ADD CONSTRAINT `user_score_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `user_score_ibfk_2` FOREIGN KEY (`project`) REFERENCES `project` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
-
---
 -- 限制表 `user_status`
 --
 ALTER TABLE `user_status`
@@ -546,7 +594,3 @@ ALTER TABLE `version_comment`
 ALTER TABLE `wit`
   ADD CONSTRAINT `wit_ibfk_1` FOREIGN KEY (`project`) REFERENCES `project` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
   ADD CONSTRAINT `wit_ibfk_2` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
