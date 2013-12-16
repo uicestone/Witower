48987679ec16cc2c65b28d7df24223446a663fba
陆秋石 <uicestone@gmail.com>
2013-12-16 10:20:40 +0800

    fix: 一处权限显示错误

--- application/views/admin/sidebar.php
+++ application/views/admin/sidebar.php
@@ -10,10 +10,10 @@
 <?if($this->user->isLogged('company')){?>
 				<li<?if(uri_segment(2)==='company'){?> class="active"<?}?>><a href="/<?=uri_segment(1)?>/company">企业管理</a></li>
 <?}?>
-<?if($this->user->isLogged('company')){?>
+<?if($this->user->isLogged('user')){?>
 				<li<?if(uri_segment(2)==='user'){?> class="active"<?}?>><a href="/<?=uri_segment(1)?>/user">用户管理</a></li>
 <?}?>
-<?if($this->user->isLogged('company')){?>
+<?if($this->user->isLogged('product')){?>
 				<li<?if(uri_segment(2)==='product'){?> class="active"<?}?>><a href="/<?=uri_segment(1)?>/product">产品管理</a></li>
 <?}?>
 <?if($this->user->isLogged('project')){?>
