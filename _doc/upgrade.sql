8cf51d1177a1343bf5661356324dd704efe1f58b
陆秋石 <uicestone@gmail.com>
2014-01-19 00:10:43 +0800

    更新服务器数据库结构

--- _doc/upgrade.sql
+++ _doc/upgrade.sql
@@ -104,6 +104,6 @@ CREATE TABLE captcha (
 );
 
 ALTER TABLE  `project` CHANGE  `summary`  `summary` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
--- server upgraded
 
 ALTER TABLE `company` ADD `certificated` BOOLEAN NOT NULL ;
+-- server upgraded
