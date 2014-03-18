98004420bf2e631d8c171df059a84ca2be62ad00
陆秋石 <uicestone@gmail.com>
2014-03-19 02:19:56 +0800

    #104 企业的管理界面中，添加边栏内容链接

--- application/views/company/sidebar.php
+++ application/views/company/sidebar.php
@@ -8,23 +8,25 @@
 		</div>
 		<div class="box list">
 			<ul class="nav nav-list">
-				<li class="nav-header">寻求帮助</li>
-				<li><a href="#">如何快速发布项目活动？</a></li>
-				<li><a href="#">正等待你选出后选人</a></li>
-				<li><a href="#">三星LEX3D手机创意作品</a></li>
-				<li><a href="#">世联研究房价备案制</a></li>
-				<li><a href="#">博策堂开发商正在面临</a></li>
+				<li class="nav-header">TELL ME WHY?</li>
+				<li><a href="/wit/5#section-1">智塔平台能为合作企业做什么？</a></li>
+				<li><a href="/wit/5#section-2">为什么广告文案创意的征集要在智塔网进行？</a></li>
+				<li><a href="/wit/5#section-3">企业如何操作？</a></li>
+				<li><a href="/wit/5#section-4">联系我们？</a></li>
 				<li class="divider"></li>
 			  </ul>
 		</div>
 		<div class="box list">
 			<ul class="nav nav-list">
-				<li class="nav-header">新手帮助</li>
-				<li><a href="#">如何快速发布项目活动？</a></li>
-				<li><a href="#">正等待你选出后选人</a></li>
-				<li><a href="#">三星LEX3D手机创意作品</a></li>
-				<li><a href="#">世联研究房价备案制</a></li>
-				<li><a href="#">博策堂开发商正在面临</a></li>
+				<li class="nav-header">KNOW HOW!</li>
+				<li><a href="/wit/6#section-1">企业如何申请智塔网企业用户及通过认证？</a></li>
+				<li><a href="/wit/6#section-2">如何获得悬赏积分用于发布项目？</a></li>
+				<li><a href="/wit/6#section-3">为什么要维护产品信息？</a></li>
+				<li><a href="/wit/6#section-4">如何发布和维护项目？</a></li>
+				<li><a href="/wit/6#section-5">如何查看创意？</a></li>
+				<li><a href="/wit/6#section-6">如何对比版本及评分？</a></li>
+				<li><a href="/wit/6#section-7">如何选择中选的创意？</a></li>
+				<li><a href="/wit/6#section-8">悬赏积分如何分配？</a></li>
 				<li class="divider"></li>
 			  </ul>
 		</div>
