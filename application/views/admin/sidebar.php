35ff4eb8e123dd21bc0ac7ba1b416a9d25c3fcd9
Uice Lu <uicestone@gmail.com>
2014-04-18 10:54:21 +0800

    删除管理页面下多余的帮助链接

--- application/views/admin/sidebar.php
+++ application/views/admin/sidebar.php
@@ -22,26 +22,4 @@
 				<li class="divider"></li>
 			  </ul>
 		</div>
-		<div class="box list">
-			<ul class="nav nav-list">
-				<li class="nav-header">寻求帮助</li>
-				<li><a href="#">如何快速发布项目活动？</a></li>
-				<li><a href="#">正等待你选出后选人</a></li>
-				<li><a href="#">三星LEX3D手机创意作品</a></li>
-				<li><a href="#">世联研究房价备案制</a></li>
-				<li><a href="#">博策堂开发商正在面临</a></li>
-				<li class="divider"></li>
-			  </ul>
-		</div>
-		<div class="box list">
-			<ul class="nav nav-list">
-				<li class="nav-header">新手帮助</li>
-				<li><a href="#">如何快速发布项目活动？</a></li>
-				<li><a href="#">正等待你选出后选人</a></li>
-				<li><a href="#">三星LEX3D手机创意作品</a></li>
-				<li><a href="#">世联研究房价备案制</a></li>
-				<li><a href="#">博策堂开发商正在面临</a></li>
-				<li class="divider"></li>
-			  </ul>
-		</div>
 	</div>
