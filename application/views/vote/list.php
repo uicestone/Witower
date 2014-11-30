c25de44b1434114b63b4f8cf40dada1c42c4dc91
78852329 <78852329@qq.com>
2014-11-30 13:16:20 +0800

    投票页面样式修正

--- application/views/vote/list.php
+++ application/views/vote/list.php
@@ -1,9 +1,8 @@
-<?$this->view('header')?>
+<? $this->view('header')?>
 <?if(isset($recommended_voting_project)){?>
 	<div class="model recommend">
-		<div class="title"><h3>每日热门投票</h3></div>
 		<div class="main">
-			<div class="info">
+			<!--<div class="info">
 				<a href="/vote/<?=$recommended_voting_project['id']?>"><?=$this->image('project',$recommended_voting_project['id'],100)?></a>
 				<a class="btn btn-primary pull-right" href="/vote/<?= $recommended_voting_project['id'] ?>">我要投票</a>
 				<ul>
@@ -21,8 +20,28 @@
 					</li>
 					<li><h4>当前投票<?= count($recommended_voting_project['voters']) ?>人</h4></li>
 				</ul>
-			</div>
-			<div class="scroll-img" >
+			</div>-->
+			<div class="maxred" style="height:430px">
+		<div class="img"><?=$this->image('project',$recommended_voting_project['id'])?></div>
+		<div class="reday_r">
+			<h2><strong><a href="/vote/<?= $recommended_voting_project['id'] ?>"><?= $recommended_voting_project['name'] ?></a>(热门投票)</strong></h2>
+			<h4>项目描述</h4><?= $recommended_voting_project['summary'] ?>
+			<strong>发布企业：<?= $recommended_voting_project['company_name'] ?>
+						<?followButton($recommended_voting_project['company'])?></strong>
+			<br><strong>活动状态：<?= lang($recommended_voting_project['status']) ?></strong>
+			<br><strong>项目金额：<span><?= $recommended_voting_project['bonus'] ?></span>元<br></strong>
+			<strong>截止时间：<?= $recommended_voting_project['wit_end'] ?>
+			&nbsp;</strong>
+			<br><strong>标签：
+			<? foreach ($recommended_voting_project['tags'] as $tag) { ?>
+			<a href="#"><?= $tag ?></a></strong>
+			<? } ?><div class=""><div class="bdsharebuttonbox"><a title="分享到QQ空间" href="#" class="bds_qzone" data-cmd="qzone"></a><a title="分享到新浪微博" href="#" class="bds_tsina" data-cmd="tsina"></a><a title="分享到人人网" href="#" class="bds_renren" data-cmd="renren"></a><a title="分享到腾讯微博" href="#" class="bds_tqq" data-cmd="tqq"></a><a title="分享到网易微博" href="#" class="bds_t163" data-cmd="t163"></a><a title="分享到微信" href="#" class="bds_weixin" data-cmd="weixin"></a><a title="分享到QQ好友" href="#" class="bds_sqq" data-cmd="sqq"></a><a href="#" class="bds_more" data-cmd="more"></a></div></div>
+			<script>window._bd_share_config={"common": {"bdSnsKey": {}, "bdText": "", "bdMini": "2", "bdMiniList": false, "bdPic": "", "bdStyle": "0", "bdSize": "24"}, "share": {}};
+			with (document)
+			0[(getElementsByTagName('head')[0] || body).appendChild(createElement('script')).src = 'http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion=' + ~(-new Date() / 36e5)];</script>
+		</div>
+	</div>
+			<div class="scroll-img" style="bottom: 75px;background: none repeat scroll 0 0 #fff;height: 25px;line-height: 25px;position: absolute;text-align: center;width: 110px;" ><strong>当前参与人员</strong></div>
 			<div class="blocks" >
 			<div class="list1" style="width:1000px;">
 			<div class="left"><img src="<?=site_url()?>style/left.png" /></div>
